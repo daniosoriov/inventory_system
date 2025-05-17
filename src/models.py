cba81b7f7bbd2794bf856965f8e9b308f72a0859
Daniel Osorio <danioshi@gmail.com>
2025-05-17 14:54:59 +0200

    add docstrings for Supplier and Product models to enhance code documentation

--- src/models.py
+++ src/models.py
@@ -8,6 +8,26 @@ class Base(DeclarativeBase):
 
 
 class Supplier(Base):
+    """
+    Represents a supplier that provides products.
+
+    This class models a supplier entity in the system, holding information about
+    the supplier's name, email, contact details, and the products they provide.
+    The supplier can be associated with multiple products, which are handled using
+    a relationship with the `Product` class.
+
+    :ivar id: The identifier of the supplier, serves as the primary key.
+    :type id: int
+    :ivar name: Name of the supplier.
+    :type name: str
+    :ivar email: Email address of the supplier.
+    :type email: str
+    :ivar phone_number: The contact phone number of the supplier.
+    :type phone_number: str
+    :ivar products: A list of products associated with the supplier.
+    Deleting a supplier will also delete orphans in the `Product` association.
+    :type products: List['Product']
+    """
     __tablename__ = 'supplier'
 
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -26,6 +46,29 @@ class Supplier(Base):
 
 
 class Product(Base):
+    """
+    Represents a product entity in the application.
+
+    This class defines the structure and attributes of a product entity, including its
+    relationships to other entities.
+    It includes details such as the product name,
+    description, stock-keeping unit (SKU), price, and associated supplier.
+
+    :ivar id: Unique identifier of the product.
+    :type id: int
+    :ivar name: Name of the product. This is a required field and must not exceed 50 characters.
+    :type name: str
+    :ivar description: A detailed description of the product. This field is optional and allows up to 200 characters.
+    :type description: str
+    :ivar sku: Stock-keeping unit of the product. This is a required and unique field, indexed for optimization.
+    :type sku: str
+    :ivar price: The price of the product. Must be specified and cannot be null.
+    :type price: float
+    :ivar supplier_id: Identifier of the supplier associated with the product.
+    :type supplier_id: int
+    :ivar supplier: Relationship to the Supplier entity representing the supplier of the product.
+    :type supplier: Supplier
+    """
     __tablename__ = 'product'
 
     id: Mapped[int] = mapped_column(primary_key=True)
