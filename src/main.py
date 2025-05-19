b7d38676b865c3b29d45ee5c0685784ad6f0c7df
Daniel Osorio <danioshi@gmail.com>
2025-05-19 14:58:59 +0200

    add Pydantic models for product creation, update, and stock management

--- src/main.py
+++ src/main.py
@@ -1,55 +1,8 @@
-from fastapi import FastAPI, HTTPException, Depends
-from sqlalchemy.orm import Session
-from orm_setup import SessionLocal
-from crud import (get_supplier,
-                  create_supplier as create_supplier_crud,
-                  update_supplier as update_supplier_crud,
-                  delete_supplier as delete_supplier_crud)
-from validation.supplier import SupplierCreate, SupplierUpdate, Supplier
+from fastapi import FastAPI
+from routes.supplier import router as supplier_router
+from routes.product import router as product_router
 
 app = FastAPI()
 
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-@app.get("/suppliers/{supplier_id}")
-def read_supplier(supplier_id: int, db: Session = Depends(get_db)):
-    supplier = get_supplier(db, supplier_id)
-    if not supplier:
-        raise HTTPException(status_code=404, detail="Supplier not found")
-    return {"supplier": supplier}
-
-
-@app.post("/suppliers/")
-def create_supplier(supplier: SupplierCreate, db: Session = Depends(get_db)) -> Supplier:
-    try:
-        result = create_supplier_crud(db, name=supplier.name, email=supplier.email, phone_number=supplier.phone_number)
-        db.commit()
-        db.refresh(result)
-        return result
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-
-
-@app.put("/suppliers/{supplier_id}")
-def update_supplier(supplier_id: int, supplier: SupplierUpdate, db: Session = Depends(get_db)):
-    updated = update_supplier_crud(db, supplier_id, supplier.name, supplier.email, supplier.phone_number)
-    db.commit()
-    if not updated:
-        raise HTTPException(status_code=404, detail="Supplier not found")
-    return {'message': 'Supplier updated successfully'}
-
-
-@app.delete("/suppliers/{supplier_id}")
-def delete_supplier(supplier_id: int, db: Session = Depends(get_db)):
-    deleted = delete_supplier_crud(db, supplier_id)
-    db.commit()
-    if not deleted:
-        raise HTTPException(status_code=404, detail="Supplier not found")
-    return {'message': 'Supplier deleted successfully'}
+app.include_router(supplier_router, prefix="/suppliers", tags=["suppliers"])
+app.include_router(product_router, prefix="/products", tags=["products"])
--- src/validation/product.py
+++ src/validation/product.py
@@ -1,4 +1,65 @@
 from pydantic import BaseModel
+from models import OperationType
+
+
+class ProductCreate(BaseModel):
+    name: str
+    description: str | None = None
+    sku: str
+    price: float
+    supplier_id: int
+    stock: int
+
+    model_config = {
+        'json_schema_extra': {
+            'examples': [
+                {
+                    'name': 'Product Name',
+                    'description': 'Product Description',
+                    'sku': 'SKU12345',
+                    'price': 19.99,
+                    'supplier_id': 1,
+                    'stock': 100
+                }
+            ]
+        }
+    }
+
+
+class ProductUpdate(BaseModel):
+    name: str | None = None
+    description: str | None = None
+    sku: str | None = None
+    price: float | None = None
+
+    model_config = {
+        'json_schema_extra': {
+            'examples': [
+                {
+                    'name': 'Updated Product Name',
+                    'description': 'Updated Product Description',
+                    'sku': 'SKU12345',
+                    'price': 19.99,
+                }
+            ]
+        }
+    }
+
+
+class ProductUpdateStock(BaseModel):
+    quantity: int
+    operation: OperationType
+
+    model_config = {
+        'json_schema_extra': {
+            'examples': [
+                {
+                    'quantity': 10,
+                    'operation': OperationType.SALE
+                }
+            ]
+        }
+    }
 
 
 class Product(BaseModel):
