6e0e705b6be1a7762ca2b75dfeb09dbf3b6da6bb
Daniel Osorio <danioshi@gmail.com>
2025-05-19 15:43:16 +0200

    configure logging setup in main.py for improved logging management

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
+import logging.config
+
 from fastapi import FastAPI
 from routes.supplier import router as supplier_router
 from routes.product import router as product_router
+
+from logger import LOGGING_CONF
+
+logging.config.dictConfig(LOGGING_CONF)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
