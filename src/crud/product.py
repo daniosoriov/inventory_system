791125a4c6ec9e151d9edca1b84e45e35a3a3e4e
Daniel Osorio <danioshi@gmail.com>
2025-05-19 14:57:49 +0200

    refactor supplier operations into separate module and rename crud.py to product.py

--- src/crud/product.py
+++ src/crud/product.py
@@ -6,48 +6,6 @@ from models import Supplier, Product, ProductTransaction, OperationType
 logger = logging.getLogger()
 
 
-# Supplier operations
-@handle_exceptions()
-def create_supplier(session: Session, name: str, email: str, phone_number: str) -> Supplier:
-    new_supplier = Supplier(
-        name=name,
-        email=email,
-        phone_number=phone_number
-    )
-    session.add(new_supplier)
-    return new_supplier
-
-
-@handle_exceptions()
-def get_supplier(session: Session, supplier_id: int) -> Supplier | None:
-    supplier = session.query(Supplier).filter_by(id=supplier_id).first()
-    return supplier
-
-
-@handle_exceptions()
-def update_supplier(session: Session, supplier_id: int, name: str = None, email: str = None,
-                    phone_number: str = None) -> Supplier | None:
-    supplier = session.query(Supplier).filter_by(id=supplier_id).first()
-    if not supplier:
-        return None
-    if name:
-        supplier.name = name
-    if email:
-        supplier.email = email
-    if phone_number:
-        supplier.phone_number = phone_number
-    return supplier
-
-
-@handle_exceptions()
-def delete_supplier(session: Session, supplier_id: int) -> bool:
-    supplier = session.query(Supplier).filter_by(id=supplier_id).first()
-    if supplier:
-        session.delete(supplier)
-        return True
-    return False
-
-
 # Product operations
 @handle_exceptions()
 def create_product(session: Session,
@@ -55,12 +13,14 @@ def create_product(session: Session,
                    description: str,
                    sku: str,
                    price: float,
+                   stock: int,
                    supplier_id: int) -> Product:
     new_product = Product(
         name=name,
         description=description,
         sku=sku,
         price=price,
+        stock=stock,
         supplier_id=supplier_id,
     )
     session.add(new_product)
